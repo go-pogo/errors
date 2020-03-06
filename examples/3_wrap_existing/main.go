2d97922042635621f328f764d88255fb975a18af
roeldev <roeldev@users.noreply.github.com>
2020-03-06 20:01:54 +0100

    Update example 3

--- examples/3_wrap_existing/main.go
+++ examples/3_wrap_existing/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/roeldev/go-errs"
 )
 
-func someAction() error {
-	dest := new(struct{})
-	return json.Unmarshal([]byte("invalid json"), dest)
+func unmarshal() (struct{}, error) {
+	dest := struct{}{}
+	err := json.Unmarshal([]byte("invalid"), &dest) // this wil result in an error
+	return dest, err
 }
 
-func doSomething() error {
-	err := someAction()
-	return errs.Wrap(err)
+func run() error {
+	data, err := unmarshal()
+	if err != nil {
+		return errs.Wrap(err)
+	}
+
+	fmt.Println(data)
+	return nil
 }
 
 func main() {
-	err := doSomething()
+	err := run()
 	if err != nil {
 		fmt.Print(err)
+		os.Exit(1)
 	}
 }
