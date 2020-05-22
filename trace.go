7a64ce8eaea2066bb009c000cfef38b2dc2266b7
roeldev <roeldev@users.noreply.github.com>
2020-05-22 14:39:05 +0200

    Add TraceSkip function

--- trace.go
+++ trace.go
@@ -6,20 +6,27 @@ import (
 )
 
 // Trace wraps an existing error with information about the stack frame its
-// called with. Errors that implement the `ErrorWithStackTrace` interface add
+// called from. Errors that implement the `ErrorWithStackTrace` interface add
 // the frame to the existing stack trace. Other "simple" errors are wrapped
 // in a `traceErr` type.
 func Trace(err error) error {
+	return TraceSkip(err, 1)
+}
+
+// TraceSkip, just like Trace(), wraps an existing error with information about
+// the stack frame its called from. The stack frame is selected based on the
+// skip argument.
+func TraceSkip(err error, skip uint) error {
 	if err == nil {
 		return nil
 	}
 
 	if e, ok := err.(ErrorWithFrames); ok {
-		e.Frames().Capture(1)
+		e.Frames().Capture(skip + 1)
 		return err
 	}
 
-	frames := CaptureFrames(1, 2)
+	frames := CaptureFrames(1, skip+2)
 	return &traceErr{
 		error:  err,
 		frames: &frames,
