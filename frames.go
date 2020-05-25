34bf6de6237917c773ad564b217c9815bcab7b25
roeldev <roeldev@users.noreply.github.com>
2020-05-25 20:38:37 +0200

    Validate captured Frames + formatted printing

--- frames.go
+++ frames.go
@@ -1,10 +1,16 @@
 package errs
 
 import (
+	"fmt"
+	"strings"
+
 	"golang.org/x/xerrors"
 )
 
-const defaultFramesCapacity = 5
+const (
+	defaultFramesCapacity = 5
+	invalidFrameSuffix    = " 0]}"
+)
 
 type Frames []xerrors.Frame
 
@@ -13,15 +19,26 @@ func CaptureFrames(n uint, skip uint) Frames {
 
 	var i uint
 	for ; i < n; i++ {
-		fr.Capture(skip + i)
+		if !fr.Capture(skip + i) {
+			break
+		}
 	}
 	return fr
 }
 
 func (fr Frames) Len() uint { return uint(len(fr)) }
 
-func (fr *Frames) Capture(skip uint) {
-	*fr = append(*fr, xerrors.Caller(int(skip)+1))
+func isValidFrame(f xerrors.Frame) bool {
+	s, x := fmt.Sprintf("%+v", f), invalidFrameSuffix
+	return s[len(s)-len(x):] != x
+}
+
+func (fr *Frames) Capture(skip uint) (ok bool) {
+	f := xerrors.Caller(int(skip) + 1)
+	if ok = isValidFrame(f); ok {
+		*fr = append(*fr, f)
+	}
+	return ok
 }
 
 func (fr Frames) Format(p xerrors.Printer) {
@@ -29,6 +46,25 @@ func (fr Frames) Format(p xerrors.Printer) {
 		fr[i].Format(p)
 	}
 }
+
+func (fr Frames) String() string {
+	var p framesPrinter
+	fr.Format(&p)
+	return p.b.String()
+}
+
+// framesPrinter uses xerrors format functionality to print a list of frames.
+type framesPrinter struct{ b strings.Builder }
+
+func (p *framesPrinter) Print(args ...interface{}) {
+	_, _ = fmt.Fprint(&p.b, args...)
+}
+
+func (p *framesPrinter) Printf(format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(&p.b, format, args...)
+}
+
+func (p *framesPrinter) Detail() bool { return true }
 
 // ErrorWithFrames interfaces provide access to a stack of frames.
 type ErrorWithFrames interface {
