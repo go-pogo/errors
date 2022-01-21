e806c295212cc5e4812454f9ea61a890b6321fcc
roeldev <roeldev@users.noreply.github.com>
2022-01-21 23:38:17 +0100

    Remove Isser and Asser interfaces

--- wrap.go
+++ wrap.go
@@ -110,3 +110,17 @@ func Cause(err error) error {
 	}
 	return err
 }
+
+// Is reports whether any error in err's chain matches target. It is fully
+// compatible with both errors.Is and xerrors.Is.
+//
+// An error is considered to match a target if it is equal to that target or if
+// it implements a method Is(error) bool such that Is(target) returns true.
+func Is(err, target error) bool { return stderrors.Is(err, target) }
+
+// As is an alias of errors.As. It finds the first error in err's chain that
+// matches target, and if so, sets target to that error value and returns true.
+func As(err error, target interface{}) bool {
+	//goland:noinspection GoErrorsAs
+	return err != nil && stderrors.As(err, target)
+}
