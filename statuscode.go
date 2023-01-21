9ca8a20619af2ef279152fca7778f0051f27f73c
roeldev <roeldev@users.noreply.github.com>
2023-01-21 09:51:47 +0100

    Add StatusCode interface

---
+++ statuscode.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2023, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	"fmt"
+)
+
+// StatusCode interfaces provide access to a (http) status code.
+type StatusCode interface {
+	error
+	StatusCode() int
+}
+
+type StatusCodeSetter interface {
+	StatusCode
+	SetStatusCode(int)
+}
+
+// WithStatusCode adds a (http) status code to the error which can be retrieved
+// using GetStatusCode and may be set to a http.ResponseWriter.
+func WithStatusCode(err error, statusCode int) StatusCode {
+	if err == nil {
+		return nil
+	}
+
+	if e, ok := err.(StatusCodeSetter); ok {
+		e.SetStatusCode(statusCode)
+		return e
+	}
+
+	return &statusCodeError{
+		embedError: &embedError{error: err},
+		statusCode: statusCode,
+	}
+}
+
+// GetStatusCode returns the status code if the error implements the StatusCode
+// interface. If not, it returns 0.
+func GetStatusCode(err error) int { return GetStatusCodeOr(err, 0) }
+
+// GetStatusCodeOr returns the status code from the first found StatusCode
+// in err's error chain. If none is found, it returns the provided value or.
+func GetStatusCodeOr(err error, or int) int {
+	for {
+		if e, ok := err.(StatusCode); ok {
+			return e.StatusCode()
+		}
+		err = Unwrap(err)
+		if err == nil {
+			break
+		}
+	}
+
+	return or
+}
+
+type statusCodeError struct {
+	*embedError
+	statusCode int
+}
+
+func (e *statusCodeError) SetStatusCode(c int) { e.statusCode = c }
+func (e *statusCodeError) StatusCode() int     { return e.statusCode }
+
+// GoString prints the error in basic Go syntax.
+func (e *statusCodeError) GoString() string {
+	return fmt.Sprintf(
+		"*statusCodeError{statusCode: %d, embedErr: %#v}",
+		e.statusCode,
+		e.error,
+	)
+}
---
+++ statuscode_test.go
@@ -0,0 +1,72 @@
+// Copyright (c) 2023, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	stderrors "errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithStatusCode(t *testing.T) {
+	for name, wantErr := range provideErrors(true) {
+		t.Run(name, func(t *testing.T) {
+			haveErr := WithStatusCode(wantErr, http.StatusOK)
+			assert.Exactly(t, http.StatusOK, GetStatusCode(haveErr))
+			assert.ErrorIs(t, haveErr, wantErr)
+
+			// update existing exitcode
+			t.Run("update", func(t *testing.T) {
+				haveErr2 := WithStatusCode(haveErr, http.StatusContinue)
+				assert.Exactly(t, http.StatusContinue, GetStatusCode(haveErr2))
+				assert.Same(t, haveErr, haveErr2)
+			})
+		})
+	}
+
+	t.Run("nil", func(t *testing.T) {
+		assert.Exactly(t, nil, WithStatusCode(nil, http.StatusNotFound))
+	})
+}
+
+func TestGetStatusCodeOr(t *testing.T) {
+	tests := map[string]struct {
+		err  error
+		or   int
+		want int
+	}{
+		"nil": {
+			err:  nil,
+			or:   http.StatusOK,
+			want: http.StatusOK,
+		},
+		"std error": {
+			err:  stderrors.New("std err"),
+			or:   http.StatusContinue,
+			want: http.StatusContinue,
+		},
+		"std error with exit code": {
+			err:  WithStatusCode(stderrors.New("std err"), http.StatusAccepted),
+			want: http.StatusAccepted,
+		},
+		"error": {
+			err:  New("some error without exit code"),
+			or:   http.StatusNotFound,
+			want: http.StatusNotFound,
+		},
+		"error with exit code": {
+			err:  WithStatusCode(New("bar"), http.StatusFound),
+			want: http.StatusFound,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Exactly(t, tc.want, GetStatusCodeOr(tc.err, tc.or))
+		})
+	}
+}
