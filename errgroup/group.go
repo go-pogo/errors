03cf6823d0e211f431633dd8f3eaec640f96b88f
roeldev <roeldev@users.noreply.github.com>
2025-02-27 16:38:32 +0100

    Update errgroup context logic to match that of golang.org/x/sync/errgroup

--- errgroup/group.go
+++ errgroup/group.go
@@ -14,29 +14,36 @@ import (
 
 var _ errlist.ErrorLister = (*Group)(nil)
 
-// A Group is a collection of goroutines working on subtasks that are part
+// Group is a collection of goroutines working on subtasks that are part
 // of the same overall task. It collects possible errors returned from the
 // subtasks.
+//
+// It is similar to [golang.org/x/sync/errgroup.Group], the main difference is
+// this [Group] collects all returned non-nil errors from the functions passed
+// to [Group.Go].
 type Group struct {
 	cancel func(error)
 	wg     sync.WaitGroup
 	errs   errlist.List
 }
 
-// WithContext returns a new Group and an associated context.Context derived
-// from ctx. Unlike golang.org/x/sync/errgroup.Group, the derived context is not
-// canceled the first time a function passed to Group.Go returns a non-nil
-// error.
+// WithContext returns a new [Group] and an associated [context.Context] derived
+// from ctx. It is similar to [golang.org/x/sync/errgroup.WithContext].
+//
+// The derived [context.Context] is canceled the first time a function passed to
+// [Group.Go] returns a non-nil error or the first time [Group.Wait] returns,
+// whichever occurs first.
 func WithContext(ctx context.Context) (*Group, context.Context) {
 	ctx, cancel := context.WithCancelCause(ctx)
 	return &Group{cancel: cancel}, ctx
 }
 
-// ErrorList returns a List of collected errors from the called goroutines.
+// ErrorList returns an [errlist.List] of collected errors from the called
+// functions passed to [Group.Go].
 func (g *Group) ErrorList() *errlist.List { return &g.errs }
 
-// Wait blocks until all function calls from the Go method have returned, then
-// returns all collected errors as a (multi) error.
+// Wait blocks until all function calls from the [Group.Go] method have
+// returned, then returns all collected errors as a (multi) error.
 func (g *Group) Wait() error {
 	g.wg.Wait()
 
@@ -48,17 +55,24 @@ func (g *Group) Wait() error {
 }
 
 // Go calls the given function in a new goroutine. Errors from all calls are
-// collected, combined and returned by Wait.
+// collected, combined and returned by [Group.Wait].
+//
+// The first call to return a non-nil error cancels the [Group]'s context, if
+// it was created by calling [WithContext]. The error will be returned by
+// [Group.Wait].
 func (g *Group) Go(fn func() error) {
 	g.wg.Add(1)
 	go func() {
+		defer g.wg.Done()
 		if err := fn(); err != nil {
 			if errors.IsCause(err) {
 				g.errs.AppendUnique(err)
 			} else {
 				g.errs.Append(err)
 			}
+			if g.cancel != nil {
+				g.cancel(err)
+			}
 		}
-		g.wg.Done()
 	}()
 }
--- errgroup/group_test.go
+++ errgroup/group_test.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/go-pogo/errors"
 	"github.com/go-pogo/errors/errlist"
@@ -15,14 +16,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWithContext(t *testing.T) {
+func TestGroup(t *testing.T) {
 	internal.DisableTraceStack()
 	defer internal.EnableTraceStack()
 
+	someErr := errors.New("some err")
 	canceledCtx, cancel := context.WithCancel(context.Background())
 	cancel()
 
-	t.Run("one ctx canceled", func(t *testing.T) {
+	t.Run("unique error", func(t *testing.T) {
 		wg, ctx := WithContext(canceledCtx)
 		wg.Go(func() error { return ctx.Err() })
 		wg.Go(func() error { return ctx.Err() })
@@ -31,7 +33,7 @@ func TestWithContext(t *testing.T) {
 		assert.Equal(t, 1, wg.ErrorList().Len())
 		assert.ErrorIs(t, err, context.Canceled)
 	})
-	t.Run("both errs", func(t *testing.T) {
+	t.Run("unique and wrapped error", func(t *testing.T) {
 		wg, ctx := WithContext(canceledCtx)
 		wg.Go(func() error { return ctx.Err() })
 		wg.Go(func() error {
@@ -42,35 +44,44 @@ func TestWithContext(t *testing.T) {
 		_ = wg.Wait()
 		assert.Equal(t, 2, wg.ErrorList().Len())
 	})
+
+	t.Run("run all goroutines", func(t *testing.T) {
+		var wg Group
+		var i int32
+
+		for n := 3; n > 0; n-- {
+			wg.Go(func() error {
+				atomic.AddInt32(&i, 1)
+				return someErr
+			})
+		}
+
+		assert.Same(t, someErr, wg.Wait())
+		assert.Exactly(t, int32(3), i)
+	})
+	t.Run("return on first error", func(t *testing.T) {
+		wg, ctx := WithContext(context.Background())
+		var i atomic.Int32
+
+		for n := 3; n > 0; n-- {
+			wg.Go(func() error {
+				time.Sleep(time.Duration(i.Load()) * time.Second)
+				if ctx.Err() == nil {
+					i.Add(1)
+					return someErr
+				}
+				return nil
+			})
+		}
+
+		haveErr := wg.Wait()
+		assert.Same(t, someErr, haveErr)
+		assert.Same(t, context.Cause(ctx), haveErr)
+		assert.NotEqual(t, int32(3), i.Load())
+	})
 }
 
-func TestWaitGroup_Go(t *testing.T) {
-	internal.DisableTraceStack()
-	defer internal.EnableTraceStack()
-
-	want := errors.New("some err")
-	var wg Group
-	var i int32
-
-	wg.Go(func() error {
-		atomic.AddInt32(&i, 1)
-		return nil
-	})
-	wg.Go(func() error {
-		atomic.AddInt32(&i, 1)
-		return want
-	})
-	wg.Go(func() error {
-		atomic.AddInt32(&i, 1)
-		return nil
-	})
-
-	have := wg.Wait()
-	assert.Exactly(t, int32(3), i)
-	assert.Same(t, want, have)
-}
-
-func TestWaitGroup_Wait(t *testing.T) {
+func TestWaitGroup_ErrorList(t *testing.T) {
 	var wg Group
 	t.Run("nil", func(t *testing.T) {
 		wg.Go(func() error {
@@ -78,6 +89,7 @@ func TestWaitGroup_Wait(t *testing.T) {
 		})
 
 		assert.Nil(t, wg.Wait())
+		assert.Exactly(t, 0, wg.ErrorList().Len())
 	})
 	t.Run("error", func(t *testing.T) {
 		internal.DisableTraceStack()
